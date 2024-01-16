60fdbb6fe93e0412f57d4b6530ef289358745e60
Federico Deniard <fededeniard@gmail.com>
2024-01-15 21:02:21 -0300

    WIP Card animations on mobile improved

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,14 +1,20 @@
+import { useState } from 'react';
 import './cards.css'
 
 
 function Card ({project, evenOrOdd, link, imgSrc, text}) {
+    const [touch, setTouch] = useState(false);
     return(
-        <a className={`card ${project} ${evenOrOdd}`}
+        <a className={`card ${project} ${evenOrOdd} ${touch ? "cardTouch" : ""}`}
             href={link}
             target='_blank'
             onContextMenu={(e) =>{
                 e.preventDefault();
             }}
+            onTouchStart={(e) => setTouch(true)}
+      onTouchEnd={(e) =>{
+          setTouch(false);
+      }}
         >
             <h5 className="title cardTitle">{project}</h5>
             <img src={imgSrc} className='image' />
